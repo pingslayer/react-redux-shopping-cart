e8fcfbb9b726da74bcf80972017463757b2cc1dd
Ronald Prajval <ronaldprajval7@gmail.com>
2021-08-27 16:52:35 +0530

    basic cart operations with redux

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,37 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 800,
+    title: "Nvidia RTX 3090ti",
+    description: "Graphics card unit by nvidia corporation",
+  },
+  {
+    id: "p2",
+    price: 200,
+    title: "AMD Ryzen 2700x",
+    description: "2nd gen Processor by AMD",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => {
+          return (
+            <ProductItem
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })}
       </ul>
     </section>
   );
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
@@ -12,9 +12,10 @@ createSlice({
       const existingItem = state.items.find((item) => {
         return item.id === newItem.id;
       });
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -25,6 +26,23 @@ createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => {
+        return item.id === id;
+      });
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => {
+          return item.id !== id;
+        });
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
